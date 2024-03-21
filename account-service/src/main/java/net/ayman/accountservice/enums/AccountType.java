ce4e5d3343f373b5a92de4ac97efd3389f78cab3
AymaNGihbidd <Aymangihbid8@outlook.com>
2024-03-21 09:18:03 +0000

    first commit

--- account-service/src/main/java/net/ayman/accountservice/enums/AccountType.java
+++
@@ -1,5 +0,0 @@
-package net.ayman.accountservice.enums;
-
-public enum AccountType {
-    CURRENT_ACCOUNT, SAVING_ACCOUNT
-}
--- account-service/src/main/java/net/ayman/accountservice/repository/BankAccountRepository.java
+++
@@ -1,7 +0,0 @@
-package net.ayman.accountservice.repository;
-
-import net.ayman.accountservice.entities.BankAccount;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BankAccountRepository extends JpaRepository<BankAccount,String> {
-}
